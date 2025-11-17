ca8f5147bb5757a7a74b6d2c89ae77d05080cbf5
wilson9108 <wilson.pulivelugula@gmail.com>
2025-11-17 17:47:07 +0530

    fixed the test bug in the backend

--- todo-backend/tests/user.test.js
+++ todo-backend/tests/user.test.js
@@ -1,20 +1,17 @@
 const request = require('supertest')
 const app = require('../app')
-const {user} = require('../models/userModel')
 
 describe('USER ROUTES',  () => {
-    beforeEach(async () => {
-        await user.create({ taskName: 'jest', taskDescription: "learning" })
-    })
 
-    it('GET /api/user should fetch all tasks',async()=>{
-        const res = await request(app).get('/api/user')
-        expect(res.statusCode).toBe(200)
-        expect(Array.isArray(res.body)).toBe(true)
-        console.log(res._body)
-    })
-    afterEach(async ()=>{
-        // await user.deleteMany({})
+    describe('Todo user api',()=>{
+        it('should signup a user', async () => {
+            const res = await request(app)
+                .post('/api/user/create')
+                .send({
+                    taskName: 'Test User',
+                })
+            expect(res.statusCode).toBe(201)
+        })  
     })
 
 })
