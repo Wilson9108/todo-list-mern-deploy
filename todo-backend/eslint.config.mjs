0e9d2f07c2a8378bf108a246b519983ec6fa0bbf
wilson9108 <wilson.pulivelugula@gmail.com>
2025-11-20 08:39:33 +0530

    created .gitignore file inthe parent folder

--- todo-backend/eslint.config.mjs
+++ todo-backend/eslint.config.mjs
@@ -1,39 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
+
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  // Frontend files (React/Vite)
-  {
-    files: ["frontend/**/*.{js,jsx,mjs,cjs}"],
-    languageOptions: {
-      globals: globals.browser, // browser globals
-      ecmaVersion: "latest",
-      sourceType: "module"
-    },
-    plugins: { js },
-    extends: ["js/recommended"],
-    rules: {
-      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      "react/react-in-jsx-scope": "off"
-    },
-    settings: {
-      react: {
-        version: "detect"
-      }
-    }
-  },
 
   // Backend files (Node + Jest)
   {
-    files: ["backend/**/*.{js,mjs,cjs}"],
+    files: ["todo-backend/**/*.{js,mjs,cjs}"],
     languageOptions: {
       globals: { ...globals.node, ...globals.jest }, // Node + Jest globals
       ecmaVersion: "latest",
       sourceType: "module"
     },
-    plugins: { js },
-    extends: ["js/recommended"],
+    extends: ["@eslint/js/recommended"], // use full package name
     rules: {
       "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
       "no-undef": "error"
--- todo-backend/index.js
+++ todo-backend/index.js
@@ -2,9 +2,8 @@ const app = require('./app')
 require('dotenv').config()
 let {dbConnect} = require('./config/dbConnect')
 dbConnect("mongodb://127.0.0.1:27017/todo")
+function getName(name){
+    return name
+}
 let {port} = process.env
-
-
-
-
 app.listen(port,()=>console.log(`Your port is running on ${port}`))
